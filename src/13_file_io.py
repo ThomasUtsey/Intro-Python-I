d4464cea1ed28ecc4629dc50762e6b3220b81a35
Thomas Utsey <tom@edgegluer.com>
2019-08-11 20:16:12 -0400

    Complete toy problems in Intro-Python-I
    Complete Scopes
    
    Complete File_io
    
    Complete Cal.py
    
    Complete classes
    
    Stretch used pep8 linter throughout development

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -1,6 +1,6 @@
 """
 Python makes performing file I/O simple. Take a look
-at how to read and write to files here: 
+at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
@@ -9,10 +9,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+file = open("src\\foo.txt", "r")
+print(file.read())
+file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+new_file = open("bar.txt", "w+")
+for line in range(10):
+    new_file.write(f"This is line {line}\n")
+new_file.close()
